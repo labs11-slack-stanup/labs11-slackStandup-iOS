4988707db2e17768ce70e945c7c9894bc765c444
Farhan Farooqui <farhan@farooqui.net>
2019-04-17 12:58:11 -0400

    refactoring curie answer view

---
+++ Curie/Curie/View Controllers/CurieCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CurieCollectionViewCell.swift
+//  Curie
+//
+//  Created by Farhan on 4/17/19.
+//  Copyright © 2019 Farhan Farooqui. All rights reserved.
+//
+
+import UIKit
+
+class CurieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var q1Label:UILabel!
+    @IBOutlet var q2Label:UILabel!
+    @IBOutlet var q3Label:UILabel!
+    
+    @IBOutlet var a1Field:UITextField!
+    @IBOutlet var a2Field:UITextField!
+    @IBOutlet var a3Field:UITextField!
+    
+    
+    
+    @IBAction func submitSurvey(_ sender: Any) {
+        
+        
+        
+    }
+    
+}
--- Curie/Curie/View Controllers/HistoryViewController.swift
+++ Curie/Curie/View Controllers/HistoryViewController.swift
@@ -12,21 +12,34 @@ class HistoryViewController: UIViewController, UserControllerContaining {
     
     var userController: UserController?
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+
+}
+
+extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0 // your number of cell here
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+    
+    
+    
 }
